d32f4152c7635ae3e6fc6e180f46fb5f01db986e
tims <tsteeman@jibes.nl>
2019-06-20 09:49:44 +0200

    experiments with loops and text writing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, omniverse!");
+    // NOTE: mut key words makes variable writable
+    // Rust variables are immutable by default.
+    let mut sum = 0;
+    for i in 0..5 {
+        let even_odd = if i % 2 == 0 {"even"} else {"odd"};
+        println!("{} {}", even_odd, i);
+        sum += i;
+    }
+    println!("The sum total: {}", sum);
+    let stdout = stdout();
+    let out = b"Hello fellow Rustaceans!";
+    let width = 24;
+    let mut writer = BufWriter::new(stdout.lock());
+
+    say(out, width, &mut writer).unwrap();
 }
