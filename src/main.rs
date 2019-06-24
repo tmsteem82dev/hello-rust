1d9baad361951f06705ac6ff1ff7ffe42bdfcd7b
Greg Kitchen <greg.kitchen@goalvault.org>
2019-06-24 17:20:20 -0500

    rustacean crawl! added some input stuff generally messing around

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,82 @@
+#[macro_use]
+extern crate prettytable;
+extern crate reqwest;
+extern crate select;
+#[macro_use] extern crate text_io; 
+
+
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::io::{stdout, stdin, BufWriter};
+use select::document::Document;
+use select::predicate::{Class, Name, Predicate};
+use prettytable::Table;
+use std::time::{Duration, Instant};
+use std::mem;
+
 
 fn main() {
+    println!("Wish this were a rustacean intro!");
+    println!("Input the number to count...");
+    //io read using text_io package
+    let count: i32 = read!();
+
     // NOTE: mut key words makes variable writable
     // Rust variables are immutable by default.
     let mut sum = 0;
-    for i in 0..5 {
-        let even_odd = if i % 2 == 0 {"even"} else {"odd"};
+
+    //crawl url but here for ferris_says scope
+    let url = String::from("https://www.fark.com");
+
+    for i in 0..count {
+        let even_odd = if i % 2 == 0 {"even"} else if i % 3 == 0 {"heck"} else {"odd"};
         println!("{} {}", even_odd, i);
         sum += i;
     }
     println!("The sum total: {}", sum);
     let stdout = stdout();
-    let out = b"Hello fellow Rustaceans!";
-    let width = 24;
+    let out = format!("^^---  Rustacean crawled: {}  ---^^", url);
+    let width = 53;
     let mut writer = BufWriter::new(stdout.lock());
 
-    say(out, width, &mut writer).unwrap();
+    // crab say, somehow happens after the crawl below
+    say(out.as_bytes(), width, &mut writer).unwrap();
+
+    // crawl web stuff, very specific for fark.com because css selectors
+    let start = Instant::now();
+    let resp = reqwest::get(url.as_str()).unwrap();
+    let document = Document::from_read(resp).unwrap();
+
+    let mut table = Table::new();
+    let mut stats_table = Table::new();
+
+    //specific crawler for fark.com
+    //using select package
+    for node in document.find(Class("headlineText")) {
+        let category = node.parent().unwrap().find(Class("headlineTopic").descendant(Name("a"))).next().unwrap().attr("title").unwrap();
+        let title = node.find(Class("headline").descendant(Name("a")))
+            .next()
+            .unwrap()
+            .text();
+        let url = node.find(Class("headline").descendant(Name("a")))
+            .next()
+            .unwrap();
+        let url_txt = url.attr("href").unwrap();
+        // shorten strings to make table aesthetically appealing
+        // otherwise table will look mangled by long URLs
+        let url_trim = url_txt.trim_left_matches('/');
+        let category_n_title = format!(" | {} | {}", category, title);
+        // FdBybl-> is formatting syntax specific to
+        // prettytable
+        table.add_row(row![FdBybl->category_n_title]);
+        table.add_row(row![Fy->url_trim]);
+    }
+    table.printstd();
+
+    //simple stats
+    let duration = start.elapsed().subsec_millis() as f64;
+    let sz = mem::size_of_val(&document);
+    let throughput = (sz as f64 / duration) * 1000.0;
+
+    stats_table.add_row(row![FdBybl->format!("Duration: {:.*}/ms | Size: {:?}/bytes | Throughput: {:.*}/kbs", 0, duration, sz, 2, throughput)]);
+    stats_table.printstd();
 }
