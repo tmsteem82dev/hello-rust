e106d277c308a8f0c198c277a27b54f0f4391b4e
Greg Kitchen <greg.kitchen@goalvault.org>
2019-06-27 18:06:21 -0500

    timing simple string parsing algorithms from an interview prep book

--- readme.md
+++ readme.md
@@ -3,6 +3,8 @@ Make sure you have:
 - Rust [installed][rustInstall]
 - Nightly Rust channel
 - VS Code with [Rust Extension Pack][vsExtPck]
+Optional:
+- [Docker](https://hub.docker.com/)
 
 ![crabby](https://mir-s3-cdn-cf.behance.net/project_modules/disp/7df0bd42774743.57ee5f32bd76e.gif)
 
@@ -36,13 +38,11 @@ Then navigate to <http://localhost:8000> to access the app.
 ## Todo
 
 - [x] Dockerize app.
-- [x] Stop parsing odd/even/heck.
+- [x] Stop parsing odd/even/heck and start doing random programming questions from [Cracking The Coding Interview][interviewcrack]
 - [x] Respond over HTTP.
 - [ ] Input list of URLs to crawl.
 - [ ] Dynamic parsing, use css-selector technique to effectively find links in any given URL.
 
 [rustInstall]: <https://www.rust-lang.org/tools/install>
 [vsExtPck]: <https://marketplace.visualstudio.com/items?itemName=swellaby.rust-pack>
-
-
-
+[interviewcrack]: <https://leonmercanti.com/books/personal-development/Cracking%20the%20Coding%20Interview%20189%20Programming%20Questions%20and%20Solutions.pdf>
--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,64 @@ use rocket::response::content::Html;
 
 fn main() {
     // rocket webserver, routes map to functions below
-    rocket::ignite().mount("/", routes![index, farkerize]).launch();
+    rocket::ignite().mount("/", routes![index, farkerize, uniquestrings]).launch();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_a() {
+        assert_eq!(all_chars_unique_hash(&String::from("abcdefg")), true);
+        assert_eq!(all_chars_unique_hash(&String::from("abcdefga")), false);
+    }
+
+    #[test]
+    fn test_part_b() {
+        assert_eq!(all_chars_unique_bits(&String::from("abcdefg")), true);
+        assert_eq!(all_chars_unique_bits(&String::from("abcdefga")), false);
+    }
+
 }
 
 #[get("/")]
 fn index() -> Html<&'static str> {
-    Html(r"<html><h1>Welcome to this Rustaceous page!</h1><br/>Go to desired fark category like /farkerize/categoryname/, case sensitive (Cool, Murica, Florida, News, etc, * for all)...<br/><a href='/farkerize/Cool'>Try it!</a><br/><footer><img src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/7df0bd42774743.57ee5f32bd76e.gif'></footer></html>")
+    Html(r"<html><h1>Welcome to this Rustaceous page!</h1><br/>Go to desired fark category like /farkerize/categoryname/, case sensitive (Cool, Murica, Florida, News, etc, * for all)...<br/><a href='/farkerize/Cool'>Try it!</a><br/><br/><br/>From the Cracking the Coding Inteview v6 book: now with random API thing to do string inspection for unique chars @ /api/unique/chars/your_string<br/><a href='/api/unique/chars/zxcvbnmasdfghjklqwertyuiop'>Try that out...</a><footer><img src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/7df0bd42774743.57ee5f32bd76e.gif'></footer></html>")
+}
+
+#[get("/api/unique/chars/<in_this_thing>")]
+fn uniquestrings(in_this_thing: &rocket::http::RawStr) -> Html<String> {
+
+    //time the method executions
+    let start_method1 = Instant::now();
+    let method1_parse = all_chars_unique_hash(in_this_thing);
+    let duration_method1 = start_method1.elapsed().subsec_nanos();
+
+    let start_method2 = Instant::now();
+    let method2_parse = all_chars_unique_bits(in_this_thing);
+    let duration_method2 = start_method2.elapsed().subsec_nanos();
+    
+    //measure the fastest result and do some string formatting
+    let mut fastest_result = String::new();
+    if duration_method1 < duration_method2 
+        { 
+            fastest_result = format!("{} returned in {:#} nanosecs by hash.<br/><br/>This was approximately {}% faster than bitfield method. (<i>{:#}</i>ns).", 
+                method1_parse.to_string(), 
+                duration_method1,
+                (duration_method2 / duration_method1) * 100,
+                duration_method2); 
+        } 
+    else 
+        { 
+            fastest_result = format!("{} returned in {:#} nanosecs by bitfield.<br/><br/>This was approximately {}% faster than hash method (<i>{:#}</i>ns).", 
+                method2_parse.to_string(), 
+                duration_method2,
+                (duration_method1 / duration_method2) * 100,
+                duration_method1); 
+        } ;
+    
+     Html(format!("<html><h2>Results for checking if all characters in string are unique...</h2>{}</html>", fastest_result))
 }
 
 #[get("/farkerize/<filter>")]
@@ -119,4 +171,38 @@ fn summarizeweb(url: String) -> Result<String, reqwest::Error> {
 
     // python api currently returning text, no parsing needed
     Ok(response.text().unwrap())
+}
+
+// grabbed from cracking the programming interview questions solved @ https://github.com/brndnmtthws/cracking-the-coding-interview-rust/tree/master/src/bin
+fn all_chars_unique_hash(s: &str) -> bool {
+    use std::collections::HashSet;
+    let mut characters: HashSet<char> = HashSet::new();
+
+    for c in s.chars() {
+        if characters.contains(&c) {
+            return false;
+        }
+        characters.insert(c);
+    }
+    true
+}
+
+// grabbed from cracking the programming interview questions solved @ https://github.com/brndnmtthws/cracking-the-coding-interview-rust/tree/master/src/bin
+fn all_chars_unique_bits(s: &str) -> bool {
+    let mut bitfield: i64 = 0;
+    let a_int_char: i16 = 'a' as i16;
+
+    for c in s.chars() {
+        let mut int_char: i16 = c as i16;
+        int_char -= a_int_char;
+
+        if (1 << int_char) & bitfield != 0 {
+            return false;
+        }
+
+        // set bit
+        bitfield |= 1 << int_char;
+    }
+
+    true
 }
